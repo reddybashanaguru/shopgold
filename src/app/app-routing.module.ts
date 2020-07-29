df1039b2c9d9ca3e63f8d478f38b7f6474221028
Naguru Reddy Basha <CE860@compassitesinc.com>
2020-07-29 12:27:25 +0530

    added tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home-tab',
+    loadChildren: () => import('./home-tab/home-tab.module').then( m => m.HomeTabPageModule)
   }
 ];
 @NgModule({
